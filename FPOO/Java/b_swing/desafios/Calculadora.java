395354c39dd728f809bc793a7c0e501bad055f5b
abehidek <hidek.abe@outlook.com>
2022-03-24 15:52:02 -0300

    switched javascript engine eval with a created eval method

--- FPOO/Java/b_swing/desafios/Calculadora.java
+++ FPOO/Java/b_swing/desafios/Calculadora.java
@@ -6,9 +6,6 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.awt.event.ActionEvent;
-
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
 
@@ -56,9 +53,10 @@ public class Calculadora extends JFrame implements ActionListener {
         setVisible(true);
     }
     public void calcular (String calculo) throws ScriptException {
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        conta = engine.eval(calculo).toString();
+        // ScriptEngineManager mgr = new ScriptEngineManager();
+        // ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        // conta = engine.eval(calculo).toString();
+        conta = Double.toString(eval(calculo));
         field.setText(conta);
     }
     @Override
@@ -86,5 +84,96 @@ public class Calculadora extends JFrame implements ActionListener {
                 e1.printStackTrace();
             }
         }
+    }
+    // https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form
+    public double eval(final String str) {
+        try {
+            return new Object() {
+                int pos = -1, ch;
+                
+                void nextChar() {
+                    ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+                }
+                
+                boolean eat(int charToEat) {
+                    while (ch == ' ') nextChar();
+                    if (ch == charToEat) {
+                        nextChar();
+                        return true;
+                    }
+                    return false;
+                }
+                
+                double parse() {
+                    nextChar();
+                    double x = parseExpression();
+                    if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                    return x;
+                }
+                
+                // Grammar:
+                // expression = term | expression `+` term | expression `-` term
+                // term = factor | term `*` factor | term `/` factor
+                // factor = `+` factor | `-` factor | `(` expression `)` | number
+                //        | functionName `(` expression `)` | functionName factor
+                //        | factor `^` factor
+                
+                double parseExpression() {
+                    double x = parseTerm();
+                    for (;;) {
+                        if      (eat('+')) x += parseTerm(); // addition
+                        else if (eat('-')) x -= parseTerm(); // subtraction
+                        else return x;
+                    }
+                }
+                
+                double parseTerm() {
+                    double x = parseFactor();
+                    for (;;) {
+                        if      (eat('*')) x *= parseFactor(); // multiplication
+                        else if (eat('/')) x /= parseFactor(); // division
+                        else return x;
+                    }
+                }
+                
+                double parseFactor() {
+                    if (eat('+')) return +parseFactor(); // unary plus
+                    if (eat('-')) return -parseFactor(); // unary minus
+                    
+                    double x;
+                    int startPos = this.pos;
+                    if (eat('(')) { // parentheses
+                        x = parseExpression();
+                        if (!eat(')')) throw new RuntimeException("Missing ')'");
+                    } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                        while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                        x = Double.parseDouble(str.substring(startPos, this.pos));
+                    } else if (ch >= 'a' && ch <= 'z') { // functions
+                        while (ch >= 'a' && ch <= 'z') nextChar();
+                        String func = str.substring(startPos, this.pos);
+                        if (eat('(')) {
+                            x = parseExpression();
+                            if (!eat(')')) throw new RuntimeException("Missing ')' after argument to " + func);
+                        } else {
+                            x = parseFactor();
+                        }
+                        if (func.equals("sqrt")) x = Math.sqrt(x);
+                        else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                        else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                        else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                        else throw new RuntimeException("Unknown function: " + func);
+                    } else {
+                        throw new RuntimeException("Unexpected: " + (char)ch);
+                    }
+                    
+                    if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+                    
+                    return x;
+                }
+            }.parse();    
+        } catch (Exception e) {
+            System.out.println("Deu Erro");
+        }
+        return 0;
     }
 }

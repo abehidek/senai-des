8afc514822c66145faf4517216338561c0d95271
abehidek <hidek.abe@outlook.com>
2022-03-31 22:49:25 -0300

    Moved repo env to root folder of repo

---
+++ shell.nix
@@ -0,0 +1,27 @@
+# Este arquivo é apenas utilizado quem utilizar o gerenciador de pacotes Nix e desejar copiar o ambiente de desenvolvimento utilizado
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+let
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  my-python = pkgs.python38;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    pygame
+    tkinter
+    pandas
+    pip
+    # other python packages you want
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-with-my-packages
+    pkgs.pfetch
+    unstable.jdk
+    pkgs.pfetch
+  ];
+  shellHook = ''
+    # maybe set more env-vars
+    export PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+  '';
+}
+

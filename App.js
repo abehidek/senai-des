5fca5514b2a35aefd0209f19ab85d843a3053ffd
abehidek <hidek.abe@outlook.com>
2022-06-06 05:05:35 -0300

    Add simple react fetching data code

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => setData(data))
+  })
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
       <TouchableOpacity style={styles.button} >Tap here</TouchableOpacity>
+      <Text>{ JSON.stringify(data) }</Text>
     </View>
   );
 }

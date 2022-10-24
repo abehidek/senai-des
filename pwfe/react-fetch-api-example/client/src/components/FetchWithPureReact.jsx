a47ab44694c0b47949ed71eb8942ee528fbe06af
abehidek <hidek.abe@outlook.com>
2022-10-24 10:25:08 -0300

    add: form submit to api on pure React

--- pwfe/react-fetch-api-example/client/src/components/FetchWithPureReact.jsx
+++ pwfe/react-fetch-api-example/client/src/components/FetchWithPureReact.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function FetchWithPureReact() {
   const [apiData, setApiData] = useState(undefined)
+  const [formData, setFormData] = useState({ name: "", author: "" })
   
   useEffect(() => {
     console.log("useEffect!")
@@ -9,11 +10,39 @@ function FetchWithPureReact() {
       .then(res => res.json())
       .then(json => setApiData(json))
   }, [])
+
+  function handleFormSubmit (event) {
+    event.preventDefault()
+    console.log(formData)
+    fetch("http://localhost:8000/livros", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
+
+  function handleFormChange (event) {
+    event.preventDefault()
+    const { name, value } = event.target
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
   
   return (
     <div>
       <p>With Pure JS</p>
       {JSON.stringify(apiData)}
+      <form onSubmit={handleFormSubmit}>
+        <input placeholder="Digite o nome do livro" name="name" type="text" onChange={handleFormChange} value={formData.name} />
+        <input placeholder="Digite o nome do autor" name="author" type="text" onChange={handleFormChange} value={formData.author} />
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
   );
 }

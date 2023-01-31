60b77aa4e50b11b1bd3b6adf052abaf7ef2d4955
abehidek <hidek.abe@outlook.com>
2023-01-30 22:29:41 -0300

    feat(flake): improve flake file by abehidek/env.nix devshell merge

--- flake.nix
+++ flake.nix
@@ -1,44 +1,51 @@
 {
-  description = "A prisma test project";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
-  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-      unstable = nixpkgs-unstable.legacyPackages.${system};
-      mpython = pkgs.python310;
-      mpython-w-pkgs = mpython.withPackages(p: with p; [
-        pygame
-        tkinter
-        pandas
-        pip
-        openpyxl
-        matplotlib
-        django
-        djangorestframework
-      ]); 
-    in {
-      devShell = pkgs.mkShell {
+  description = "Shell environments for development";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    abehidek.url = "github:abehidek/env.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    stable,
+    abehidek,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in rec {
+    devShells = forAllSystems (system: {
+      default = let
+        pkgs = nixpkgs.legacyPackages.${system};
+        node16Pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+          overlays = [(final: prev: { nodejs = prev.nodejs-16_x; })];
+        };
+        myPython = pkgs.python310;
+        myPythonWithPkgs = myPython.withPackages(p: with p; [
+          pygame
+          tkinter
+          pandas
+          pip
+          openpyxl
+          matplotlib
+          django
+          djangorestframework
+        ]);
+      in pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
+        inputsFrom = [ abehidek.devShells.${system}.nodejs ];
         buildInputs = with pkgs; [
-          mpython-w-pkgs
+          myPythonWithPkgs
           jdk
-          nodejs
-          yarn
-          nodePackages.expo-cli
-          ngrok
-          # android-studio
-          nodePackages.typescript
+          node16Pkgs.nodePackages.expo-cli
+          node16Pkgs.ngrok
         ];
-        shellHook = with pkgs; '']
-          export PYTHONPATH=${mpython-w-pkgs}/${mpython-w-pkgs.sitePackages}
-        '';
+        PYTHONPATH="${myPythonWithPkgs}/${myPythonWithPkgs.sitePackages}";
       };
     });
+  };
 }
